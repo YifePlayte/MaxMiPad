72c6d98434e4466349032969716a28c0e76a0b15
YifePlayte <yife-playte@foxmail.com>
2023-08-11 16:32:05 +0800

    1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yifeplayte.maxmipadinput"
         minSdk = 31
         targetSdk = 33
-        versionCode = 11
-        versionName = "1.3.2"
+        versionCode = 12
+        versionName = "1.3.3"
     }
 
     buildTypes {
