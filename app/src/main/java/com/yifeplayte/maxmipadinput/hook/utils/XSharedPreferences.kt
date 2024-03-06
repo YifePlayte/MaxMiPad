6f676e2ad27049e47025a4aa4e19885747236ed9
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2024-03-06 14:04:37 +0800

    Optimize XSharedPreferences utils

--- app/src/main/java/com/yifeplayte/maxmipadinput/hook/utils/XSharedPreferences.kt
+++ app/src/main/java/com/yifeplayte/maxmipadinput/hook/utils/XSharedPreferences.kt
@@ -4,28 +4,25 @@ import com.yifeplayte.maxmipadinput.BuildConfig
 import de.robv.android.xposed.XSharedPreferences
 
 object XSharedPreferences {
+    private val prefs = XSharedPreferences(BuildConfig.APPLICATION_ID, "config")
+
     fun getBoolean(key: String, defValue: Boolean): Boolean {
-        val prefs = XSharedPreferences(BuildConfig.APPLICATION_ID, "config")
-        if (prefs.hasFileChanged()) {
-            prefs.reload()
-        }
+        reload()
         return prefs.getBoolean(key, defValue)
     }
 
     fun getString(key: String, defValue: String): String {
-        val prefs = XSharedPreferences(BuildConfig.APPLICATION_ID, "config")
-        if (prefs.hasFileChanged()) {
-            prefs.reload()
-        }
+        reload()
         return prefs.getString(key, defValue) ?: defValue
     }
 
     fun getStringSet(key: String, defValue: Set<String>): Set<String> {
-        val prefs = XSharedPreferences(BuildConfig.APPLICATION_ID, "config")
-        if (prefs.hasFileChanged()) {
-            prefs.reload()
-        }
         return prefs.getStringSet(key, defValue) ?: defValue
     }
 
+    private fun reload() {
+        if (prefs.hasFileChanged()) {
+            prefs.reload()
+        }
+    }
 }
